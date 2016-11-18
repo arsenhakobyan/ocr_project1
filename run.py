015b89b4c2969841d5ffb2a9b100011836ebbe7c
arsenhakobyan <hakobyan.ars@gmail.com>
2016-11-19 01:27:46 +0400

    Updated the run.py script to write the result in json file.

--- run.py
+++ run.py
@@ -5,10 +5,19 @@ import json
 import urllib.request
 
 url = json.loads(open(sys.argv[1]).read())
+dirName = (os.path.dirname(sys.argv[1]))
+baseName = (os.path.basename(sys.argv[1]))
+outputFile = dirName + "/out_" + str(baseName)
 print (url["image_url"])
 
 urllib.request.urlretrieve(url["image_url"], "downloaded.jpg")
 cmd = "tesseract downloaded.jpg output -psm 6"
-os.system(cmd)
-f = open('output.txt', 'r')
-print (f.read())
+
+from PIL import Image
+import pytesseract
+im = Image.open("downloaded.jpg")
+text = pytesseract.image_to_string(im, config="-psm 6")
+print (text)
+dictionary = {"text": text}
+with open(outputFile, "w") as json_file:
+    json.dump(dictionary, json_file)
